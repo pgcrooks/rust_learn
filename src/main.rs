48efb97057892db771903e2a95efd6b8f4203c5d
Paul Crooks <pgcrooks@gmail.com>
2019-10-21 21:41:13 +0100

    Add character

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::mem;
 
 #[derive(Debug)]
 struct Coordinate(f32, f32, f32);
@@ -16,10 +17,26 @@ impl fmt::Display for Vector {
     }
 }
 
+// Lifetime struct
+struct Person<'a> {
+    name: &'a str,
+    coord: Coordinate,
+    vec: Vector,
+}
+impl<'a> fmt::Display for Person<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Person {} is at coordinate {} with vector {}.",
+            self.name,
+            self.coord,
+            self.vec)
+    }
+}
+
 fn transpose_coord(input: Coordinate) -> Coordinate {
     Coordinate(input.2, input.1, input.0)
 }
-
 
 fn format_fun() {
     println!("Hello, formatting!");
@@ -34,7 +51,33 @@ fn format_fun() {
     println!("Transposed: {:?}", transpose_coord(coord));
 }
 
+fn analyze_slice(slice: &[i32]) {
+    println!("  Slice[0]: {}", slice[0]);
+    println!("  Slice has {} elements", slice.len());
+}
+
+fn slice_magic() {
+    let xs: [i32; 5] = [2, 4, 6, 8, 10];
+    println!("xs first: {}", xs[0]);
+    println!("xs last:  {}", xs[4]);
+    println!("xs occupies {} bytes", mem::size_of_val(&xs));
+    println!("Big slice:");
+    analyze_slice(&xs);
+    println!("Small slice:");
+    analyze_slice(&xs[1..3]);
+}
+
+fn game() {
+    let paul = Person{
+        name: "Paul",
+        coord: Coordinate(0.0, 0.0, 0.0),
+        vec: Vector(0.0, 0.0)};
+    println!("{}", paul);    
+}
+
 
 fn main() {
-    format_fun();    
+    format_fun();
+    slice_magic();
+    game();
 }
